419735c097a07353add12c8db8e30f6a690bdb1d
Chris <chrisbcl@hotmail.com>
2021-03-14 20:41:00 +0000

    test: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,11 @@ import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResult = {
+    data: faker.random.objectElement(),
+    status: faker.random.number()
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
 const makeSut = (): AxiosHttpClient => new AxiosHttpClient()
 
@@ -19,5 +24,14 @@ describe('AxiosHttpClient', () => {
         const request = mockPostRequest()
         await sut.post(request)
         expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    })
+
+    it('should return the correct statusCode and body', async () => {
+        const sut = makeSut()
+        const response = await sut.post(mockPostRequest())
+        expect(response).toEqual({
+            statusCode: mockedAxiosResult.status,
+            body: mockedAxiosResult.data
+        })
     })
 })
