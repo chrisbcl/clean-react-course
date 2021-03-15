206876157f0214bd9f4f48850ac2afa5fa90ec0a
Chris <chrisbcl@hotmail.com>
2021-03-15 02:13:11 +0000

    feature: add login view

---
+++ src/presentation/components/spinner/Spinner.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Spinner.styles.scss'
+
+type SpinnerProps = React.HTMLAttributes<HTMLElement>
+
+const Spinner = (props: SpinnerProps): JSX.Element => {
+    return (
+        <div {...props} className={[styles.Spinner, props.className].join(' ')}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Spinner
