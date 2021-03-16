c64a386f4c30c4611d3f21629e272e04a7ab2979
Chris <chrisbcl@hotmail.com>
2021-03-16 01:03:31 +0000

    test: ensure Login starts with inputs with required state

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -5,9 +5,19 @@ import { Login } from '..'
 describe('<Login />', () => {
     it('should start with correct initial state', () => {
         const { getByTestId } = render(<Login />)
+
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
+
         const submitButton = getByTestId('submit') as HTMLButtonElement
         expect(submitButton.disabled).toBe(true)
+
+        const emailStatus = getByTestId('email-status')
+        expect(emailStatus.title).toBe('Required')
+        expect(emailStatus.textContent).toBe('🔴')
+
+        const passwordStatus = getByTestId('password-status')
+        expect(passwordStatus.title).toBe('Required')
+        expect(passwordStatus.textContent).toBe('🔴')
     })
 })
