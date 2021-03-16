2189df5c34dca1b051b4a4a8f12066de31ba8487
Chris <chrisbcl@hotmail.com>
2021-03-16 19:03:59 +0000

    test: ensure Login calls Validation with correct test

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -47,7 +47,7 @@ describe('<Login />', () => {
         expect(passwordStatus.textContent).toBe('🔴')
     })
 
-    it('should call validation with the correct value', () => {
+    it('should call validation with the correct email', () => {
         const {
             sut: { getByTestId },
             validationSpy
@@ -57,6 +57,19 @@ describe('<Login />', () => {
         fireEvent.input(emailInput, { target: { value: 'any_email' } })
         expect(validationSpy.input).toEqual({
             email: 'any_email'
+        })
+    })
+
+    it('should call validation with the correct password', () => {
+        const {
+            sut: { getByTestId },
+            validationSpy
+        } = makeSut()
+
+        const passwordInput = getByTestId('password')
+        fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+        expect(validationSpy.input).toEqual({
+            password: 'any_password'
         })
     })
 })
