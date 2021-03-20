8613fbe098b0f08315f099f6ea20b52f65f14cff
Chris <chrisbcl@hotmail.com>
2021-03-20 16:50:07 +0000

    chore: change jest config to ignore index files with export statements only

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
         '<rootDir>/src/**/*.{ts,tsx}',
         '!<rootDir>/src/main/**/*',
         '!<rootDir>/src/presentation/components/router/**/*',
+        '!<rootDir>/src/**/index.ts',
+        '!<rootDir>/src/presentation/contexts/**/*',
         '!**/*.d.ts'
     ],
     coverageDirectory: 'coverage',
