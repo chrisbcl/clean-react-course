20f24ea9ba460f4fad236339e2bcf4cb5d2db5ca
Chris <chrisbcl@hotmail.com>
2021-03-20 21:48:09 +0000

    feature: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/addaccount/remote-add-account.ts
+++ src/data/usecases/addaccount/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
-import { EmailInUseError } from '@/domain/errors'
+import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
 
@@ -13,10 +13,12 @@ export class RemoteAddAccount implements AddAccount {
         const response = await this.httpClient.post({ url: this.url, body: params })
 
         switch (response.statusCode) {
+            case HttpStatusCode.Ok:
+                return response.body as AccountModel
             case HttpStatusCode.Forbidden:
                 throw new EmailInUseError()
+            default:
+                throw new UnexpectedError()
         }
-
-        return response.body as AccountModel
     }
 }
