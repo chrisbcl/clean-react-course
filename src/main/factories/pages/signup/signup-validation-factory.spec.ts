5999d25b11d952ad8374507958b3600de5755f62
Chris <chrisbcl@hotmail.com>
2021-03-21 14:54:45 +0000

    test: add SignupValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { ValidationBuilder } from '@/validation/validators/validationbuilder/validation-builder'
+import { ValidationComposite } from '@/validation/validators/validationcomposite/validation-composite'
+import { makeSignupValidation } from './signup-validation-factory'
+
+describe('SignupValidationFactory', () => {
+    it('should make ValidationComposite with correct validations', () => {
+        const composite = makeSignupValidation()
+        expect(composite).toEqual(
+            ValidationComposite.build([
+                ...ValidationBuilder.field('name').required().min(5).build(),
+                ...ValidationBuilder.field('email').required().email().build(),
+                ...ValidationBuilder.field('password').required().min(5).build(),
+                ...ValidationBuilder.field('passwordConfirmation').required().min(5).sameAs('password').build()
+            ])
+        )
+    })
+})
